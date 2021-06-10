9acd013e9ede4fd5ce1bd9bb683a1c31f22aaccf
Nefonfo <victorarmenta30@gmail.com>
2021-06-10 15:37:20 -0500

    fix z-index in other libraries

--- src/js/components/calendar_container.js
+++ src/js/components/calendar_container.js
@@ -35,19 +35,19 @@ const calendar_container = (id, color, text_color, evt_selected) => {
         root.replaceChild(calendar_grid(GLOBAL_DATE, color, text_color, evt_selected), root.childNodes[2]);
     }
     
-    root.classList.add('z-10', 'mx-2', 'md:mx-0', 'overflow-y-auto','w-full', 'md:w-1/3', 'flex', 'flex-wrap', `bg-${color}${(color === 'white' || color === 'black') ? '': '-400'}`, 'shadow-2xl', 'rounded-md');
+    root.classList.add('z-50', 'mx-2', 'md:mx-0', 'overflow-y-auto','w-full', 'md:w-1/3', 'flex', 'flex-wrap', `bg-${color}${(color === 'white' || color === 'black') ? '': '-400'}`, 'shadow-2xl', 'rounded-md');
     root.appendChild(calendar_title(`${GLOBAL_DATE.format('YYYY')} - ${GLOBAL_DATE.format('MMMM').toUpperCase()}`, color, text_color, month_listener));
     root.appendChild(days_header(text_color));
     root.appendChild(calendar_grid(GLOBAL_DATE, color, text_color, evt_selected));
 
     let modal = document.createElement('div');
     modal.id = id;
-    modal.classList.add('fixed', 'w-full', 'h-full', 'top-0', 'left-0', 'flex', 'items-center', 'justify-center');
+    modal.classList.add('z-50', 'fixed', 'w-full', 'h-full', 'top-0', 'left-0', 'flex', 'items-center', 'justify-center');
     modal.style = 'overflow-x: hidden; overflow-y: visible !important;';
 
     let modal_over = document.createElement('div');
     modal_over.id = id + '_modal';
-    modal_over.classList.add('modal-overlay', 'cursor-pointer','absolute', 'w-full', 'h-full', 'bg-gray-900', 'opacity-50'); 
+    modal_over.classList.add('z-50', 'modal-overlay', 'cursor-pointer','absolute', 'w-full', 'h-full', 'bg-gray-900', 'opacity-50'); 
     
     modal.appendChild(modal_over);
     modal.appendChild(root);
